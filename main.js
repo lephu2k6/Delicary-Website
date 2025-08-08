6341f5a2f3fd1bff2039bcebc8dd646c3c8ca245
Minh Phú <lephu422@hotmail.com>
2025-08-08 09:33:30 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,26 +1,26 @@
-// //swiper
-// var swiper = new Swiper(".home", {
-//         spaceBetween: 30,
-//         centeredSlides: true,
-//         autoplay: {
-//           delay: 2500,
-//           disableOnInteraction: false,
-//         },
-//         pagination: {
-//           el: ".swiper-pagination",
-//           clickable: true,
-//         },
-//         navigation: {
-//           nextEl: ".swiper-button-next",
-//           prevEl: ".swiper-button-prev",
-//         },
-//       });
+//swiper
+var swiper = new Swiper(".home", {
+        spaceBetween: 30,
+        centeredSlides: true,
+        autoplay: {
+          delay: 2500,
+          disableOnInteraction: false,
+        },
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+      });
 
 
 
-//   const menuIcon = document.querySelector('.menu-icon');
-//   const navbar = document.querySelector('.navbar');
+  const menuIcon = document.querySelector('.menu-icon');
+  const navbar = document.querySelector('.navbar');
 
-//   menuIcon.onclick = () => {
-//     navbar.classList.toggle('active');
-//   };
+  menuIcon.onclick = () => {
+    navbar.classList.toggle('active');
+  };
